bd3930bb5fcd4ad79c40247d81089ef6251b85fd
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2025-04-07 23:14:12 +0800

    added

--- services/gSheetsService.js
+++ services/gSheetsService.js
@@ -3,7 +3,7 @@ const { google } = require('googleapis');
 const auth = new google.auth.GoogleAuth({
     credentials: {
         client_email: process.env.VERCEL_GOOGLE_CLIENT_EMAIL,
-        private_key: process.env.VERCEL_GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        private_key: process.env.VERCEL_GOOGLE_PRIVATE_KEY,
     },
     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
