b653905b456fa86a4172774c7bf39b0cce5af3d7
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2025-04-10 14:28:29 +0800

    added

--- services/mailersetup.js
+++ services/mailersetup.js
@@ -9,7 +9,7 @@ const cred = (message, clientEmail, clientName) => {
         html: `<!DOCTYPE html>
             <html lang="en">
             <head>
-            <meta charset="UTF-8">
+            <meta charset="UTF-8"> 
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Service Inquiry</title>
             </head>
